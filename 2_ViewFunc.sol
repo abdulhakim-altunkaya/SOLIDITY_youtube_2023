63eddf0cfccb00efdda4fd6a5fa78ade8e77ac5d
abdulhakim_altunkaya <altunkaya2@gmail.com>
2023-01-12 21:46:02 +0300

    view and write functions uploaded

---
+++ 2_ViewFunc.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.7;
+
+contract ViewFunc {
+
+    string internal myCity = "Nusaybin";
+
+    function getCity() external view returns(string memory) {
+        return myCity;
+    }
+}
