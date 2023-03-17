efac8b792fa93fc2be5615f44b3f6992fb2025b9
abdulhakim_altunkaya <altunkaya2@gmail.com>
2023-03-18 00:03:12 +0300

    constant-immutable first file created

---
+++ 65_constant_immutable.sol
@@ -0,0 +1,27 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.10;
+
+contract Apple {
+    
+
+
+
+/*
+
+****CONSTANT****
+All the instances of the variables are replaced with value. 
+Replacement is done during compilation.
+There will not be a storage allocation, as there will be only value.
+Value type variables and STRINGS can have "constant" keyword. No array, struct,...
+Variable name should be CAPITAL
+
+****IMMUTABLE****
+All the instances of the variables are replaced with value. 
+Replacement is done during construction. 
+There will not be a storage allocation, as there will be only value.
+Only 32 bytes are reserved for this. For this reason, only Value type variables can have "immutable" keyword. No string, array, struct,...
+Variable name should be CAPITAL
+
+*/
+}
