13d6c2e00de1f8ba0741954831b946c777db12df
abdulhakim_altunkaya <altunkaya2@gmail.com>
2023-03-24 11:24:04 +0300

    library file first version is finished

---
+++ 67_libraries.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.7;
+
+contract Apple{
+
+
+/*
+  - Libraries are used to avoid repetitive functions in contracts
+  - Libraries cannot be deployed, they cannot have state variables
+  - Libraries can only have pure/view functions
+  - Libraries cannot inherit and cannot be inherited
+  - Libraries cannot have fallback/receive and payable functions
+  - First parameter of the library function becomes the one on which the function is invoked. 
+  The remaining parameters go inside the function call.
+*/
+
+
+} 
